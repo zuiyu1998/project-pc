a16e82b5db8a16faa7a66a03a36177e853f2a76d
zuiyu1998 <1542844298@qq.com>
2024-01-26 16:13:37 +0800

    ✨ feat(editor): 添加实体统计和帧数统计

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -1,10 +1,36 @@
-use bevy::prelude::*;
-use bevy_editor_pls::prelude::*;
+use bevy::{
+    diagnostic::{EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+};
+use bevy_editor_pls::{
+    controls::{Action, Binding, BindingCondition, Button, EditorControls, UserInput},
+    prelude::*,
+};
 
 pub struct InternalEditorPlugin;
 
 impl Plugin for InternalEditorPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(EditorPlugin::default());
+        app.add_plugins((
+            EditorPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+            EntityCountDiagnosticsPlugin,
+        ))
+        .insert_resource(res_editor_controls());
     }
+}
+
+fn res_editor_controls() -> EditorControls {
+    let mut editor_controls = EditorControls::default_bindings();
+    editor_controls.unbind(Action::PlayPauseEditor);
+
+    editor_controls.insert(
+        Action::PlayPauseEditor,
+        Binding {
+            input: UserInput::Single(Button::Keyboard(KeyCode::Escape)),
+            conditions: vec![BindingCondition::ListeningForText(false)],
+        },
+    );
+
+    editor_controls
 }
